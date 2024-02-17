c5807dfe3d2e642cd8e22be8c51db25d1a988064
Max Schmidt <61661405+SchmidtMax-staips@users.noreply.github.com>
2024-02-17 23:42:27 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Library to communicate with the Viessmann ViCare API",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/somm15/PyViCare",
+    url="https://github.com/SchmidtMax-staips/PyViCare",
     packages=setuptools.find_packages(exclude=["tests"]),
     install_requires=['Authlib>=1.2.0'],
     setup_requires=['setuptools-git-versioning<1.8.0'],
